3f788a3ed2533f93ad4ee53f410c6208af0931e1
NarunAswin <narunaswin@gmail.com>
2024-02-11 11:52:12 +0530

    rename

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
+import image1 from './mainlogo/logo.jpeg'
 
 function App() {
   return (
     <div className="App">
       <div id="backgroundimage">
         <div id="navbar">
+          <img src={image1}/>
           <input placeholder='enter search'/>
         <Link to={"Signin"}>SIGNIN</Link>
         <Link to={"about"}>HOME</Link>
         <Link to={"about"}>IMAGE</Link>
         <Link to={"about"}>LOGINOUT</Link>
         </div>
-        <div id="logo"></div>
       </div>
    
     </div>
