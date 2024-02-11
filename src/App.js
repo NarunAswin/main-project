6a4cc36d17ece7d8c2ca88f05bf38ff75f13ae3a
NarunAswin <narunaswin@gmail.com>
2024-02-11 15:46:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
-import image1 from './mainlogo/logo.jpeg'
+import image1 from './mainlogo/logo2.jpg'
+
 
 function App() {
   return (
     <div className="App">
       <div id="backgroundimage">
         <div id="navbar">
-          <img src={image1}/>
-          <input placeholder='enter search'/>
-        <Link to={"Signin"}>SIGNIN</Link>
-        <Link to={"about"}>HOME</Link>
-        <Link to={"about"}>IMAGE</Link>
-        <Link to={"about"}>LOGINOUT</Link>
+          <img src={image1} id="logo" />
+          <ul>
+          <li><Link className='navbarcolor' to={"Signin"}>SIGNIN</Link></li>
+          <li> <Link  className='navbarcolor' to={"about"}>HOME</Link></li>
+          <li> <Link className='navbarcolor' to={"about"}>IMAGE</Link></li>
+          <li><Link className='navbarcolor' to={"about"}>LOGINOUT</Link></li>
+        </ul>
+        <div id='searchbox'>
+        <input placeholder='search enter' />
+        </div>
         </div>
       </div>
-   
     </div>
   );
 }
