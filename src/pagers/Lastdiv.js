a4e21eb6c2dffcf1f0c867a2273e9c5a09e6ceca
NarunAswin <narunaswin@gmail.com>
2024-03-01 08:01:45 -0800

    update

--- src/pagers/Lastdiv.js
+++ src/pagers/Lastdiv.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Lastdiv.css';
+import image2 from './main1/WhatsApp Image 2024-02-13 at 10.25.33 PM (1)-removebg-preview.jpg'
+import socialmedia from './main1/icons8-facebook-48.png'
 
 
 function Lastdiv() {
   return (
     <div id='lastdiv'>
+      <br></br>
+      <div id='logodiv'>
+        <img src={image2} />
+        <div id='para'>
+          <p>Chennai's premier Badminton training academy and largest chain of indoor sports venues for Badminton, Swimming, Cricket, Futsal and other sports featuring world-class Kawasaki branded indoor badminton courts, a freshwater indoor swimming pool and an indoor venue for cricket, futsal, table tennis, archery and more.</p>
+          <p>Chennai's premier Badminton training academy and largest chain of indoor sports venues for Badminton, Swimming, Cricket, Futsal and other sports featuring world-class Kawasaki branded indoor badminton courts, a freshwater indoor swimming pool and an indoor venue for cricket, futsal, table tennis, archery and more.</p>
+        </div>
+        <div id='socialmedia'>
+          
+        </div>
+
+      </div>
+
 
     </div>
   )
